cebe168b4e0453b530b603a5643de4b5961bb55f
basiima <mrbasiima@gmail.com>
2022-09-08 14:22:15 +0300

    add speaker section to main layout

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
+import SpeakerSection from "@/components/SpeakerSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <AboutSection />
+      <SpeakerSection />
     </main>
   );
 }
