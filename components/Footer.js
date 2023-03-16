cc4a025bd5e177812a94699b10d2d1b193311dbe
Basiima Julius <mrbasiima@gmail.com>
2023-03-16 16:52:21 +0300

    change 'class' to 'className' & svg props to JSX

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,10 @@ import logo from '../img/logo.png';
 
 export default function Footer() {
     return(
-      <footer class="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
+      <footer className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
         <div className="max-w-5xl mx-auto px-4">
-        <div class="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <div class="my-10">
+        <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <div className="my-10">
             <Image
               src={logo}
               alt="Pycon logo"
@@ -18,43 +18,43 @@ export default function Footer() {
             This is going to be the FIRST PyCon Conference in Uganda!
             </p>
         </div>
-        <div class="my-10">
+        <div className="my-10">
             <h6
-            class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+            className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
             Useful links
             </h6>
-            <p class="mb-4">
-            <a href="/" class="text-neutral-600 dark:text-neutral-200"
+            <p className="mb-4">
+            <a href="/" className="text-neutral-600 dark:text-neutral-200"
                 >Home</a
             >
             </p>
-            <p class="mb-4">
-            <a href="#!" class="text-neutral-600 dark:text-neutral-200"
+            <p className="mb-4">
+            <a href="#!" className="text-neutral-600 dark:text-neutral-200"
                 >About Us</a
             >
             </p>
-            <p class="mb-4">
-            <a href="#!" class="text-neutral-600 dark:text-neutral-200"
+            <p className="mb-4">
+            <a href="#!" className="text-neutral-600 dark:text-neutral-200"
                 >Tickets</a
             >
             </p>
             <p>
-            <a href="#!" class="text-neutral-600 dark:text-neutral-200"
+            <a href="#!" className="text-neutral-600 dark:text-neutral-200"
                 >Privacy Policy</a
             >
             </p>
         </div>
-        <div class="my-10">
+        <div className="my-10">
             <h6
-            class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+            className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
             Contact
             </h6>
-            <p class="mb-4 flex items-center justify-center md:justify-start">
+            <p className="mb-4 flex items-center justify-center md:justify-start">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="mr-3 h-5 w-5">
+                className="mr-3 h-5 w-5">
                 <path
                 d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
                 <path
@@ -62,12 +62,12 @@ export default function Footer() {
             </svg>
             MoTIV Bugolobi
             </p>
-            <p class="mb-4 flex items-center justify-center md:justify-start">
+            <p className="mb-4 flex items-center justify-center md:justify-start">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="mr-3 h-5 w-5">
+                className="mr-3 h-5 w-5">
                 <path
                 d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                 <path
@@ -75,39 +75,39 @@ export default function Footer() {
             </svg>
             pyconug@pycon.org
             </p>
-            <p class="mb-4 flex items-center justify-center md:justify-start">
+            <p className="mb-4 flex items-center justify-center md:justify-start">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="mr-3 h-5 w-5">
+                className="mr-3 h-5 w-5">
                 <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
-                clip-rule="evenodd" />
+                clipRule="evenodd" />
             </svg>
             + 256 701 599 722
             </p>
-            <p class="flex items-center justify-center md:justify-start">
+            <p className="flex items-center justify-center md:justify-start">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="mr-3 h-5 w-5">
+                className="mr-3 h-5 w-5">
                 <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
-                clip-rule="evenodd" />
+                clipRule="evenodd" />
             </svg>
             + 256 789 189 583
             </p>
         </div>
         </div>
     </div>
-    <div class="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
+    <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
         <span>© 2023 Copyright:</span>
         <a
-        class="font-semibold text-neutral-600 dark:text-neutral-400"
+        className="font-semibold text-neutral-600 dark:text-neutral-400"
         href="https://ug.pycon.org/"
         >PyconUg</a
         >
