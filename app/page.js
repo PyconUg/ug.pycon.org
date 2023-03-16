3c65741c7c0b6f1f7a794151825eb74ce91feb3b
Basiima Julius <mrbasiima@gmail.com>
2023-03-16 17:59:53 +0300

    import HeroSection component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Pycon Uganda</h1>
+      <HeroSection />
     </main>
   );
 }
