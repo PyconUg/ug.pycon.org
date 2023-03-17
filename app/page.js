1298760cd90b25f4eb1ce70b76be46739bfc4ca5
Basiima Julius <mrbasiima@gmail.com>
2023-03-17 06:59:59 +0300

    import AboutSection component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }
