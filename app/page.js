8ca11222bda1c330a82a2d7edb7be7d0d19fa18d
basiima <mrbasiima@gmail.com>
2023-03-22 10:58:28 +0300

    add venue section to main layout

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import SpeakerSection from "@/components/SpeakerSection";
 import ScheduleSection from "@/components/ScheduleSection";
+import VenueSection from "@/components/VenueSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <AboutSection />
       <SpeakerSection />
       <ScheduleSection />
+      <VenueSection />
     </main>
   );
 }
