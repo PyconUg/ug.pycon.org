03fece6eabc02e462b5e25cb630e51c56246ed29
basiima <mrbasiima@gmail.com>
2023-03-23 07:02:34 +0300

    add buy tickets section

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import ScheduleSection from "@/components/ScheduleSection";
 import VenueSection from "@/components/VenueSection";
 import SponsorSection from "@/components/SponsorSection";
 import NewsletterSection from "@/components/NewsletterSection";
+import BuyTicketSection from "@/components/BuyTicketSection";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <VenueSection />
       <SponsorSection />
       <NewsletterSection />
+      <BuyTicketSection />
     </main>
   );
 }
