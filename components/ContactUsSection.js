8092ffbf6307c7e4f87307b2c9a55f8351a428db
Basiima Julius <mrbasiima@gmail.com>
2023-03-25 09:45:28 +0300

    Update components/ContactUsSection.js
    Co-authored-by: Hassan Bahati  <65954740+HassanBahati@users.noreply.github.com>

--- components/ContactUsSection.js
+++ components/ContactUsSection.js
@@ -44,7 +44,7 @@ export default function ContactUsSection() {
                             <input type="text" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 block" placeholder="Let us know how we can help" required />
                         </div>
                         <div className="sm:col-span-2">
-                            <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Email</label>
+                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Email</label>
                             <textarea className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 block" placeholder="Leave a comment"></textarea>
                         </div>
                         <button type="submit" className="bg-[#ffe135] text-white py-2 px-6 m-2 border rounded-3xl">Send message</button>
