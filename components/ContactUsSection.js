d10a2e1ba3c3521dd2f6e52b0cc44bbc2dc3c8be
Basiima Julius <mrbasiima@gmail.com>
2023-03-25 09:43:44 +0300

    Update components/ContactUsSection.js
    Co-authored-by: Hassan Bahati  <65954740+HassanBahati@users.noreply.github.com>

--- components/ContactUsSection.js
+++ components/ContactUsSection.js
@@ -40,7 +40,7 @@ export default function ContactUsSection() {
                             <input type="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 block" placeholder="name@email.com" required />
                         </div>
                         <div>
-                            <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Email</label>
+                            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Email</label>
                             <input type="text" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 block" placeholder="Let us know how we can help" required />
                         </div>
                         <div className="sm:col-span-2">
