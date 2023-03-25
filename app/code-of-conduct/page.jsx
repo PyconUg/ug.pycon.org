9886c71b31eb7726701648d43b45ffc412be310e
HassanBahati <mukisabahati@gmail.com>
2023-03-25 09:01:03 +0300

    feat: move code of conduct section 1 into /app/code-of-conduct/page.jsx

--- app/code-of-conduct/page.jsx
+++ app/code-of-conduct/page.jsx
@@ -1,3 +1,29 @@
 export default function CodeOfConduct() {
-    return (<>code of conduct</>)
+  return (
+    <>
+      {" "}
+      <section class="flex bg-cyan-900 text-zinc-300">
+        <div className="flex py-10 lg:items-center">
+          <div className="mx-auto text-center">
+            <h1 className="text-3xl font-extrabold sm:text-5xl text-white">
+              Code of Conduct
+            </h1>
+            <p className="mt-4 sm:text-xl text-justify mx-5">
+              PyCon Uganda is a community conference intended for networking and
+              collaboration in the developer community. We value the
+              participation of each member of the Python community and want all
+              attendees to have an enjoyable and fulfilling experience.
+              Accordingly, all attendees are expected to show respect and
+              courtesy to other attendees throughout the conference and at all
+              conference events. To make clear what is expected, all staff,
+              attendees, speakers, exhibitors, organizers, and volunteers at any
+              PyCon event are required to conform to the following Code of
+              Conduct, as set forth by the Python Software Foundation.
+              Organizers will enforce this code throughout the event.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
