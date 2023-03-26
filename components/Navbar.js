0e1576cd5560d571d97da5b6d1de8bea63bf45e5
Basiima Julius <mrbasiima@gmail.com>
2023-03-26 11:05:45 +0300

    Feature/link logo to homepage (#12)
    Link to homepage via logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -38,7 +38,9 @@ export default function Navbar() {
     <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <Image src={logo} alt="Pycon logo" width={120} height={70} />
+          <Link href={"/"}>
+            <Image src={logo} alt="Pycon logo" width={120} height={70} />
+          </Link>
           <div className="flex space-x-4 text-gray-900">
             {NavbarLinks.map(({ path, label }) => {
               return <a href={path}>{label}</a>;
