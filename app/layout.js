77ffae64ff2081a90ff02611732f74a460f1f06b
Hassan Bahati <65954740+HassanBahati@users.noreply.github.com>
2023-03-30 09:52:54 +0300

    fix: Make navbar responsive  (#14)

--- app/layout.js
+++ app/layout.js
@@ -10,10 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="">
-        <Navbar/>
+      <body className="flex min-h-screen flex-col justify-between">
+        <Navbar />
         <main className="">{children}</main>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
