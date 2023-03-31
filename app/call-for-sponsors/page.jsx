e431454c3263cfacbe316a74825a3ca41068287f
basiima <mrbasiima@gmail.com>
2023-03-31 18:13:51 +0300

    refactor: replace escape character

--- app/call-for-sponsors/page.jsx
+++ app/call-for-sponsors/page.jsx
@@ -279,7 +279,7 @@ export default function CallForSponsors() {
           </div>
           <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Interested in sponsoring? We'd love to hear from you.</p>
+      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Interested in sponsoring? We would love to hear from you.</p>
       <form action="mailto:pyconug@gmail.com" method="post" enctype="multipart/form-data" className="space-y-8">
           <div>
               <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Name</label>
