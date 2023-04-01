e858a4898e22054cb80fc81c071bad434b453edc
basiima <mrbasiima@gmail.com>
2023-04-01 14:33:47 +0300

    fix route to sponsors page

--- components/SponsorSection.js
+++ components/SponsorSection.js
@@ -8,7 +8,7 @@ export default function SponsorSection() {
                     <h2 className="text-4xl text-[#0e1b4d] font-bold uppercase">Our Sponsors</h2>
                     <p className="text-base p-4 font-medium">Join our growing list of sponsors who are making PyCon Uganda happen</p>
                     <div className="py-12">
-                        <Link className="bg-[#ffe135] text-white font-bold py-3 px-10 m-2 border rounded-3xl" href={"/call-for-sponsors"}>
+                        <Link className="bg-[#ffe135] text-white font-bold py-3 px-10 m-2 border rounded-3xl" href={"/sponsors"}>
                             Become a Sponsor
                         </Link>
                     </div>
