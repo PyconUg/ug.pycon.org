1eb2080c941791418951f7b86ab066b2776a2fbc
HassanBahati <mukisabahati@gmail.com>
2023-04-01 10:11:55 +0300

    chore: add event venvue

--- components/VenueSection.js
+++ components/VenueSection.js
@@ -1,15 +1,19 @@
 export default function VenueSection() {
-    return (
-        <section>
-            <div className="py-[60px]">
-                <div className="mx-auto max-w-screen-xl px-4 py-16 lg:flex lg:h-60 lg:items-center">
-                    <div className="mx-auto max-w-xl text-center">
-                    <h2 className="text-4xl text-[#0e1b4d] font-bold uppercase">Event Venue</h2>
-                    <p className="text-base p-0 font-medium">Event venue location info and gallery</p>
-                    <h5 className="text-xl font-bold m-2">To be announced</h5>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section>
+      <div className="py-[60px]">
+        <div className="mx-auto max-w-screen-xl px-4 py-16 lg:flex lg:h-60 lg:items-center">
+          <div className="mx-auto max-w-xl text-center">
+            <h2 className="text-4xl text-[#0e1b4d] font-bold uppercase">
+              Event Venue
+            </h2>
+            <p className="text-base p-0 font-medium">
+              <b>PyCon Uganda</b> will be hosted at MOTIV Kampala
+            </p>
+            <h5 className="text-xl font-bold m-2">To be announced</h5>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
