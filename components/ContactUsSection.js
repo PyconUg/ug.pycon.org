12c229e6379549873b730a9e12bda6a3593b022f
HassanBahati <mukisabahati@gmail.com>
2023-04-01 14:40:04 +0300

    fix: add correct email address

--- components/ContactUsSection.js
+++ components/ContactUsSection.js
@@ -25,7 +25,7 @@ export default function ContactUsSection() {
             <i className="bi bi-envelope"></i>
             <h3 className="text-lg uppercase font-bold">Email</h3>
             <p>
-              <a href="mailto:info@example.com">pyconug@pycon.org</a>
+              <a href="mailto:pyconug@pycon.org">pyconug@pycon.org</a>
             </p>
           </div>
         </div>
