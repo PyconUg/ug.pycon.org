5e15444a92130b6d5e917c27b16154a4e6011c27
HassanBahati <mukisabahati@gmail.com>
2023-04-01 10:17:48 +0300

    style: make map width full

--- components/VenueSection.js
+++ components/VenueSection.js
@@ -3,7 +3,7 @@ export default function VenueSection() {
     <section>
       <div className="py-[60px]">
         <div className=" px-4 py-4 items-center">
-          <div className="mx-auto max-w-xl text-center">
+          <div className="mx-auto max-w-xl text-center space-y-4">
             <h2 className="text-4xl text-[#0e1b4d] font-bold uppercase">
               Event Venue
             </h2>
@@ -11,7 +11,7 @@ export default function VenueSection() {
               <b>PyCon Uganda</b> will be hosted at MOTIV Kampala
             </p>
           </div>
-          <div className="">
+          <div className="mt-4">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.755633380876!2d32.604737214325!3d0.32118129976566917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177dbbbf5945eecb%3A0x8346c5f2c6f8ba41!2sMOTIV%20Uganda!5e0!3m2!1sen!2sug!4v1680332840375!5m2!1sen!2sug"
               width="100%"
