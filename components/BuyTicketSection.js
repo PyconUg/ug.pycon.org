827d44e32ffbbf8e5946ae76be4c5bb191dcf926
HassanBahati <mukisabahati@gmail.com>
2023-04-01 14:45:24 +0300

    fix: add ticket link

--- components/BuyTicketSection.js
+++ components/BuyTicketSection.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 export default function BuyTicketSection() {
+    const QUICKET_LINK = "https://qkt.io/pyconug";
     return (
         <section className="py-[60px] bg-[#f6f7fd]">
             <div className="mx-auto max-w-screen-xl px-4 py-16 lg:flex lg:h-60 lg:items-center">
@@ -8,7 +9,7 @@ export default function BuyTicketSection() {
                     <h2 className="text-4xl text-[#0e1b4d] font-bold uppercase">Buy Tickets</h2>
                     <p className="text-base p-0 font-medium">Tickets can be bought by following the link below.</p>
                     <div className="py-12">
-                        <Link className="bg-[#ffe135] text-white font-bold py-3 px-10 m-2 border rounded-3xl" href={"/sponsors"}>
+                        <Link className="bg-[#ffe135] text-white font-bold py-3 px-10 m-2 border rounded-3xl" href={QUICKET_LINK}>
                             Buy a Ticket
                         </Link>
                     </div>
