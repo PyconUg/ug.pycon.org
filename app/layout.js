7c07949b119af9f884ee6ff806689c1994da7775
Amanya-Brian <amanyabrian54@gmail.com>
2023-04-01 22:51:38 +0300

    Analytics: Added google tag

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+      {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TKTWYJW41C"></script>
+        <script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-TKTWYJW41C');
+          `}
+        </script>
+
       <body className="flex min-h-screen flex-col justify-between">
         <Navbar />
         <main className="">{children}</main>
