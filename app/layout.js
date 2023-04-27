24e1bdcdf664d672ba5c4f51199b0c2c3e6145eb
Amanya-Brian <amanyabrian54@gmail.com>
2023-04-27 11:34:28 +0300

    Edited Google analytics

--- app/layout.js
+++ app/layout.js
@@ -9,25 +9,27 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <>
     <html lang="en">
 
       {/* <!-- Google tag (gtag.js) --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TKTWYJW41C"></script>
-        <script>
-          {`
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-TKTWYJW41C"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html:`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-TKTWYJW41C');
-          `}
-        </script>
-
+          gtag('config', 'G-TKTWYJW41C');`,
+        }}
+      />
       <body className="flex min-h-screen flex-col justify-between">
         <Navbar />
         <main className="">{children}</main>
         <Footer />
       </body>
     </html>
+    </>
   );
 }
