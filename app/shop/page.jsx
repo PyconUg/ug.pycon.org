46457bdef18df9181cedda0a80557303d57644c6
HassanBahati <mukisabahati@gmail.com>
2023-06-27 15:54:31 +0300

    style: mobile responsiveness

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -20,7 +20,7 @@ export default function Shop() {
           </div>
         </div>
       </section>
-      <section className="mx-8 md:mx-28 max-w-screen-xl my-10 text-justify text-lg mb-40">
+      <section className=" md:mx-28 max-w-screen-xl my-10 text-justify text-lg mb-40">
         <Card
           image="shop/hoodie.jpg"
           title="PyConUg Hoodie"
