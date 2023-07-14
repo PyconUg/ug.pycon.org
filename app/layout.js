39b858578386a89a1ee13ed2ac2b5b0e2e590943
HassanBahati <mukisabahati@gmail.com>
2023-07-14 19:52:59 +0300

    chore: move images location to assets

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "@/components/ui/navbar";
+import Footer from "@/components/ui/footer";
 import "./globals.css";
 
 export const metadata = {
--- app/page.js
+++ app/page.js
@@ -1,27 +1,5 @@
-import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
-import SpeakerSection from "@/components/SpeakerSection";
-import ScheduleSection from "@/components/ScheduleSection";
-import VenueSection from "@/components/VenueSection";
-import SponsorSection from "@/components/SponsorSection";
-import NewsletterSection from "@/components/NewsletterSection";
-import BuyTicketSection from "@/components/BuyTicketSection";
-import ContactUsSection from "@/components/ContactUsSection";
-import DiversityPage from './financial-aid/page';
+import HomeComponent from "@/components/home";
 
-export default function Home() {
-  return (
-    <>
-      <HeroSection />
-      <AboutSection />
-      <SpeakerSection />
-      {/* <ScheduleSection /> */}
-      <VenueSection />
-      <SponsorSection />
-      <NewsletterSection />
-      <BuyTicketSection />
-      {/* <DiversityPage/> We have a separate page for this, no need to include it on home page */}
-      <ContactUsSection />
-    </>
-  );
+export default function HomePage() {
+  return <HomeComponent />;
 }
