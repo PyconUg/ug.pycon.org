59a4eac2843508632af1ce8e7f320e08fd9f3e13
HassanBahati <mukisabahati@gmail.com>
2023-07-14 19:52:20 +0300

    chore: add color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,20 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js"
+    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pyconug-yellow": "#F1DB2F",
+        "pyconug-lightBlue": "#177E89",
+        "pyconug-darkblue": "#094C61",
+        "pyconug-red": "#E32E26",
+      },
+    },
   },
-  plugins: [require("tw-elements/dist/plugin")]
-}
+  plugins: [require("tw-elements/dist/plugin")],
+};
