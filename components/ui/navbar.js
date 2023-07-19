5418df0bea2ac5ebd8b57679baa15c19cb6faea0
Amanya Brian <65342608+Amanya-Brian@users.noreply.github.com>
2023-07-19 22:46:12 +0300

    Added hamburger menu for smaller devices (#69)

--- components/ui/navbar.js
+++ components/ui/navbar.js
@@ -1,7 +1,10 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "./logo";
 import { QUICKET_LINK } from "@/utils/constants";
+import { useState } from "react";
 
 export default function Navbar() {
   const NavbarLinks = [
@@ -32,12 +35,43 @@ export default function Navbar() {
   const SHOP_ACTION_LABEL = "Shop";
   const SHOP_ACTION_LINK = "/shop";
 
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
+  const toggleSideMenu = () => {
+    setIsSideMenuOpen(!isSideMenuOpen);
+  };
+
   return (
     <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
       <div className="mx-[6%] my-2">
         <div className="flex items-center justify-between h-16">
           <Logo />
-          <div className="md:flex space-x-4 text-gray-900 hidden">
+
+          {/* Side Menu - For smaller devices */}
+          <div className="md:hidden flex items-center">
+            <button
+              className="mr-2 text-gray-700 focus:outline-none"
+              onClick={toggleSideMenu}
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            </button>
+          </div>
+
+          {/* For larger devices */}
+          <div className="md:flex space-x-4 hidden">
             {NavbarLinks.map(({ path, label }, index) => {
               return (
                 <a key={index} href={path}>
@@ -45,8 +79,6 @@ export default function Navbar() {
                 </a>
               );
             })}
-          </div>
-          <div className="flex flex-row space-x-4">
             <Link
               className="border-black hidden md:block  px-6 py-1 border rounded-lg"
               href={SHOP_ACTION_LINK}
@@ -58,10 +90,39 @@ export default function Navbar() {
               href={QUICKET_LINK}
             >
               {TICKET_BTN_LABEL}
-            </Link>{" "}
+            </Link>
+          </div>
+
+          {/* Side Menu Links - For smaller devices */}
+          <div
+            className={`${
+              isSideMenuOpen ? "block" : "hidden"
+            } md:hidden absolute bg-pyconug-lightBlue text-white top-20 right-0`}
+          >
+            {NavbarLinks.map(({ path, label }, index) => {
+              return (
+                <a key={index} href={path} className="block px-4 py-2">
+                  {label}
+                </a>
+              );
+            })}
+            <Link
+              href={SHOP_ACTION_LINK}
+              className="block px-4 py-2 border-gray-300"
+            >
+              {SHOP_ACTION_LABEL}
+            </Link>
+            <Link
+              href={QUICKET_LINK}
+              className="block px-4 py-2 border-gray-300"
+            >
+              {TICKET_BTN_LABEL}
+            </Link>
           </div>
         </div>
       </div>
     </nav>
   );
 }
+
+
