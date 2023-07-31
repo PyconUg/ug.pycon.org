8a67428794adfd924891051ef6cd02aa264e59c2
HassanBahati <mukisabahati@gmail.com>
2023-08-01 01:13:01 +0300

    feat: speakers pages

---
+++ components/ui/sections/page-section.jsx
@@ -0,0 +1,7 @@
+export default function PageSection(params) {
+  return (
+    <div className="pt-8">
+      <p className="text-base text-justify text-gray-900">{params.text}</p>
+    </div>
+  );
+}
