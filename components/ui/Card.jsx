ee07cb9e9cc700a479a16eaf108754b0fda51cee
HassanBahati <mukisabahati@gmail.com>
2023-08-01 01:24:52 +0300

    fix: bug

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -92,7 +92,7 @@ export default function Card(params) {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
               stroke-linecap="round"
