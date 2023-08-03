e9f06a345d8ec39a0eee195d42a80b5f9d25dfcf
HassanBahati <mukisabahati@gmail.com>
2023-08-03 09:04:33 +0300

    docs: add schedule

--- components/schedule/data.js
+++ components/schedule/data.js
@@ -6,54 +6,152 @@ export const Tabs = [
 
 export const PYCON_DAY1_SCHEDULE = [
   {
-    title: "Opening Keynotes",
-    duration: "09:00 - 09:30",
-    speaker: "John Doe",
+    title: "Opening Keynote",
+    duration: "09:00 - 10:00",
+    speaker: "",
   },
   {
-    title: "Break Out Sessions",
-    duration: "09:00 - 09:30",
-    speaker: "Moses Mark",
+    title: "Breakfast",
+    duration: "10:00 - 11:00",
+    speaker: "",
   },
   {
-    title: "What is Pycon",
-    duration: "09:00 - 09:30",
-    speaker: "James Karen",
+    title:
+      "Enhancing Python Application Scalability: Infrastructure, CI/CD, Testing",
+    duration: "11:00 - 11:30",
+    speaker: "Abigail Afi Gbadago",
+  },
+  {
+    title:
+      "Introduction to Data Science with Python: Unleashing the Power of Data",
+    duration: "11:30 - 12:00",
+    speaker: "Emmanuel Okiria",
+  },
+  {
+    title: "Machine Learning Experimentation with DVC and VS Code",
+    duration: "12:00 - 12:30",
+    speaker: "Gift Ojeabulu",
+  },
+  {
+    title:
+      "Python Custom Visuals in Power BI: Unleashing the Power of Data Visualization",
+    duration: "12:30 - 13:00",
+    speaker: "Allan Kiche",
+  },
+  {
+    title: "Lunch Break",
+    duration: "13:00 - 14:00",
+    speaker: "",
+  },
+  {
+    title: "Panel Session (PROSEIT)",
+    duration: "14:00 - 15:00",
+    speaker: "",
+  },
+  {
+    title: "Developing IoT Applications with Python Microcontrollers",
+    duration: "15:00 - 15:30",
+    speaker: "Adewale Olaniyan",
+  },
+  {
+    title:
+      "SIGN-TO-TEXT:Unleashing the Power of Google AI for real time ASL transcription",
+    duration: "15:30 - 16:00",
+    speaker: "Atieno Ouma",
+  },
+  {
+    title: "Closing Keynote",
+    duration: "16:00 - 17:00",
+    speaker: "",
+  },
+  {
+    title: "Lightning Talks",
+    duration: "17:00 - 17:30",
+    speaker: "",
   },
 ];
 
 export const PYCON_DAY2_SCHEDULE = [
   {
-    title: "Opening Keynotes",
-    duration: "09:00 - 09:30",
-    speaker: "John Doe",
+    title: "Opening Keynote",
+    duration: "09:00 - 10:00",
+    speaker: "",
   },
   {
-    title: "Python IDEs",
-    duration: "09:00 - 09:30",
-    speaker: "Moses Mark",
+    title: "Breakfast",
+    duration: "10:00 - 10:30",
+    speaker: "",
   },
   {
-    title: "Python Overview",
-    duration: "09:00 - 09:30",
-    speaker: "James Karen",
+    title:
+      "Python for Open Source Machine Learning Projects: Insights from Outreachy Internships",
+    duration: "11:00 - 11:30",
+    speaker: "Hellen Namulinda and Kirabo Kakopo Atuhurira",
+  },
+  {
+    title: "Word Embeddings: A Pythonic Delight",
+    duration: "11:30 - 12:00",
+    speaker: "Brayan Kai Mwanyumba",
+  },
+  {
+    title:
+      "Harnessing the Power of Real-time Data Processing with Apache Beam and Python",
+    duration: "12:00 - 12:30",
+    speaker: "Mwangi Kabiru",
+  },
+  {
+    title: "Automation & Integration Using Python",
+    duration: "12:30 - 13:00",
+    speaker: "Wesley Kambale",
+  },
+
+  {
+    title: "Lunch break",
+    duration: "13:00 - 14:00",
+    speaker: "",
+  },
+  {
+    title: "Panel Session (Diversity and Inclusion)",
+    duration: "14:00 - 15:00",
+    speaker: "",
+  },
+  {
+    title: "Using Python and Dash plotly to make interactive dashboard",
+    duration: "15:00 - 15:30",
+    speaker: "Abdulhaq Adetunji Salako",
+  },
+  {
+    title:
+      "Revolutionize Your Python Development with GitHub Copilot: Tips, Tricks, and Best Practices",
+    duration: "15:30 - 16:00",
+    speaker: "Olayinka Atobiloye",
+  },
+  {
+    title: "Closing Keynote",
+    duration: "16:00 - 17:00",
+    speaker: "",
+  },
+  {
+    title: "Lightning Talks",
+    duration: "17:00 - 17:30",
+    speaker: "",
+  },
+  {
+    title: "Outreachy 1000 Intern Celebrations",
+    duration: "17:30 - 20:00",
+    speaker: "",
   },
 ];
 
 export const PYCON_DAY3_SCHEDULE = [
   {
-    title: "Opening Keynotes",
-    duration: "09:00 - 09:30",
-    speaker: "John Doe",
+    title: "Opening Keynote",
+    duration: "09:00 - 10:00",
+    speaker: "",
   },
   {
-    title: "Intro into parallel programming with python",
-    duration: "09:00 - 09:30",
-    speaker: "Moses Mark",
-  },
-  {
-    title: "Background of python",
-    duration: "09:00 - 09:30",
-    speaker: "James Karen",
+    title: "Breakfast",
+    duration: "10:00 - 10:30",
+    speaker: "",
   },
 ];
