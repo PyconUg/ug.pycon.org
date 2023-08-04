af3672cc40c04844f5c8fc49e0a2f0fdf84a14be
HassanBahati <mukisabahati@gmail.com>
2023-08-05 01:47:36 +0300

    style: add styles to by ticket button

--- components/home/sections/buy-ticket.js
+++ components/home/sections/buy-ticket.js
@@ -2,19 +2,26 @@ import { QUICKET_LINK } from "@/utils/constants";
 import Link from "next/link";
 
 export default function BuyTicket() {
-    return (
-        <section className="py-[60px] bg-[#f6f7fd]">
-            <div className="mx-auto max-w-screen-xl px-4 py-16 lg:flex lg:h-60 lg:items-center">
-                <div className="mx-auto max-w-xl text-center">
-                    <h2 className="text-4xl text-[#0e1b4d] font-bold uppercase">Buy Tickets</h2>
-                    <p className="text-base p-0 font-medium">Tickets can be bought by following the link below.</p>
-                    <div className="py-12">
-                        <Link className="bg-[#ffe135] text-white font-bold py-3 px-10 m-2 border rounded-3xl" href={QUICKET_LINK}>
-                            Get Ticket
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="py-[60px] bg-[#f6f7fd]">
+      <div className="mx-auto max-w-screen-xl px-4 py-16 lg:flex lg:h-60 lg:items-center">
+        <div className="mx-auto max-w-xl text-center">
+          <h2 className="text-4xl text-[#0e1b4d] font-bold uppercase">
+            Buy Tickets
+          </h2>
+          <p className="text-base p-0 font-medium">
+            Tickets can be bought by following the link below.
+          </p>
+          <div className="py-12">
+            <Link
+              className="uppercase text-white bg-pyconug-lightBlue px-12 py-4 rounded-md w-64 font-bold "
+              href={QUICKET_LINK}
+            >
+              Get Ticket
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
