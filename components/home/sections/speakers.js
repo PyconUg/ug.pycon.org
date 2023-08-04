6a742eae01c00b729adf45366837854531cf8047
HassanBahati <mukisabahati@gmail.com>
2023-08-05 02:42:20 +0300

    style: make speakers section responsive

--- components/home/sections/speakers.js
+++ components/home/sections/speakers.js
@@ -12,7 +12,7 @@ export default function Speakers() {
           return <SpeakerCard speaker={speaker} key={index} />;
         })}
       </section>
-      <section className="flex md:flex-row flex-col md:space-x-4 justify-center">
+      <section className="flex md:flex-row  flex-col md:space-x-4 space-y-16 text-center md:space-y-0 justify-center">
         <div>
           <Link
             className=" uppercase text-white bg-pyconug-lightBlue border-2 hover:bg-white hover:text-pyconug-lightBlue hover:border-pyconug-lightBlue px-12 py-4 rounded-md w-64 font-bold "
