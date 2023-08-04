4129f5adb4115d342dcccbc276d97f10e6fcf128
HassanBahati <mukisabahati@gmail.com>
2023-08-05 01:59:42 +0300

    feat: update speakes on home page

--- components/home/sections/speakers.js
+++ components/home/sections/speakers.js
@@ -1,44 +1,27 @@
-import Image from 'next/image';
+"use client";
+import SpeakerCard from "@/components/speakers/speaker-card";
+import { SPEAKERS } from "@/components/speakers/speakers-data";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Speakers() {
   return (
-    <section>
-      <div className="mx-auto max-w-screen-xl px-4 py-16 lg:flex lg:h-90 lg:items-center">
-        <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-4xl text-[#0e1b4d] font-bold uppercase mb-10">
-            Event Speakers
-          </h2>
-          <p className="text-base p-0 font-medium">
-            We shall start with guidelines for the Call For Proposals(CFP).
-            Peruse through so that you can prepare before we officially open
-            submissions!
-            <br />
-            This will be a formal invitation for individuals or organizations to
-            submit proposals to speak at the Pycon Uganda conference. It allows
-            conference organizers to identify a diverse range of potential
-            speakers with unique perspectives and experiences. This will help to
-            ensure that the conference features a well-rounded program that is
-            engaging and relevant to attendees. This will also be crucial in
-            promoting diversity and inclusion in the conference program and
-            encourage the sharing of new ideas and approaches.
-          </p>
-          <div className="flex justify-center mt-4 pb-2">
-            <p className="flex flex-col text-center md:flex-row">
-              <span>
-                Interested in speaking at <b>PyCon Uganda 2023</b> ?
-              </span>
-              &nbsp;
-            </p>
-          </div>
-              
-          <div style={{
-              display: "flex",
-              justifyContent: "center",
-            }}>
-              <Image src="/assets/images/closed.png" alt="CFP closed" width={164} height={84} />
-              </div>
+    <main className="space-y-14 mt-20">
+      <section className="grid md:grid-cols-2 lg:grid-cols-3  grid-cols-1 mx-10 gap-y-10">
+        {SPEAKERS?.slice(0, 3)?.map((speaker, index) => {
+          return <SpeakerCard speaker={speaker} key={index} />;
+        })}
+      </section>
+      <section className="flex flex-row justify-center">
+        <div>
+          <Link
+            className=" uppercase text-white bg-pyconug-lightBlue px-12 py-4 rounded-md w-64 font-bold "
+            href={"/speakers"}
+          >
+            View all spakers
+          </Link>
         </div>
-      </div>
-    </section>
+      </section>
+    </main>
   );
 }
