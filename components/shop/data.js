c3b693f735923f3d71d84e723ac4aac76cf93aee
HassanBahati <mukisabahati@gmail.com>
2023-09-19 14:00:04 +0300

    feat: add quicket steps

--- components/shop/data.js
+++ components/shop/data.js
@@ -5,29 +5,39 @@ export const PURCHASE_INSTRUCTIONS = [
   "Should you have any questions or need further assistance, please contact the Chairperson of the Organizing Committee at <b>+256 781 666 410 / +256 757 866 121</b>.",
 ];
 
-export const TIV_PURCHASE_STEPS = [
+export const QUICKET_PURCHASE_STEPS = [
   {
     step: "Step 1",
-    action: "Visit the TIV offices during working hours.",
+    action:
+      "Visit the quicket page at <a target='_blank' href='https://www.quicket.co.ug/events/218513-pycon-uganda-2023/#/'>click here</a>.",
   },
   {
     step: "Step 2",
     action:
-      "Go to the designated merchandise counter at TIV Block B Reception and find <b>NASSIWA NORAH</b> or reach her on <b>+256 750 647059.</b>",
+      "Click the action button labelled <b>Tickets</b> to proceed to the next step.",
   },
   {
     step: "Step 3",
     action:
-      "Select the items you wish to purchase and provide the required details to her.",
+      "If you have a Promo Code, add it in the field labelled <b>Enter Promo Code</b> and Enter to <b>Apply</b> the code to get a discount. If you dont have a Promo Code, please proceed to the next step.",
   },
   {
     step: "Step 4",
     action:
-      "Make the payment using the available payment methods (cash or mobile money).",
+      "Click the increase or decrease buttons to add the number of tickets you wish to get and then click <b>Next</b>.",
   },
   {
     step: "Step 5",
-    action: "Obtain a receipt as proof of your purchase.",
+    action: "Enter your personal details and click <b>Next</b>.",
+  },
+  {
+    step: "Step 6",
+    action: "Enter your personal details and click <b>Next</b>.",
+  },
+  {
+    step: "Step 7",
+    action:
+      "Finally you will be redirected to the <b>Checkout Page</b> wherer you will enter the email address to receive your ticket. After adding it, Please follow the next prompts to make your payment.",
   },
 ];
 
