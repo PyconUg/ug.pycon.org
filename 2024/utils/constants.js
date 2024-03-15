9f8d06ad61d2592a281c182f0cec6ceb92962361
Hassan Bahati <65954740+HassanBahati@users.noreply.github.com>
2024-03-16 00:36:26 +0300

    Fix: 2024 diversity financial aid link (#118)

--- 2024/utils/constants.js
+++ 2024/utils/constants.js
@@ -1,3 +1,4 @@
 export const VENUE = `NWSC International Resource Centre (IREC)`;
 export const CONFERENCE_DATE = `9th - 13th October, 2024`;
 export const CFP_LINK = `https://docs.google.com/forms/d/e/1FAIpQLSeGwFKzPU_ItBpepm-kK1fyiDKECp06H8lrgDtMGNGBIeCXYQ/viewform?vc=0&c=0&w=1&flr=0`;
+export const DIVERSITY_FINANCIAL_AID_LINK = `https://bit.ly/pyconUG-2024-diversity-aid`;
