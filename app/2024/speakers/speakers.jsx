7c94ec84d78d023f2e972ae43f9aa9326d9e41a8
HassanBahati <mukisabahati@gmail.com>
2024-05-01 17:41:09 +0300

    refactor: dynamically show speakers and a message if speaker list is empty

--- app/2024/speakers/speakers.jsx
+++ app/2024/speakers/speakers.jsx
@@ -1,18 +1,53 @@
+"use client";
+import { SPEAKERS_2024 } from "@/2024/utils/constants";
+import SpeakerCard from "@/components/speakers/speaker-card";
+import Link from "next/link";
+
 export default function Speakers() {
   return (
     <>
-      {" "}
-      <section className="flex bg-cyan-900 text-zinc-300">
-        <div className="flex py-10 lg:items-center">
-          <div className="md:mx-10 text-center mx-4">
+      <section className=" bg-cyan-900 text-zinc-300">
+        <div className="flex flex-col justify-center items-center h-[35vh]">
+          <div>
             <h1 className="text-3xl font-extrabold sm:text-5xl text-white">
               Speakers
             </h1>
-            <p className="mt-4 sm:text-xl text-justify mx-5">
-              We shall be releasing the list of speakers soon.
+          </div>
+
+          <div>
+            {" "}
+            <p className="text-base p-4 font-medium">
+              Discover the Voices of PyCon Uganda 2024!
             </p>
           </div>
         </div>
+      </section>
+
+      <section>
+        {SPEAKERS_2024?.length == 0 ? (
+          <div className="my-14">
+            <span>We shall be releasing the list of speakers soon!</span>
+          </div>
+        ) : (
+          <>
+            {" "}
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 my-16 grid-cols-1 mx-10 gap-y-10">
+              {SPEAKERS_2024?.map((speaker, index) => {
+                return <SpeakerCard speaker={speaker} key={index} />;
+              })}
+            </div>
+            <div className="flex md:flex-row  flex-col md:space-x-4 space-y-16 text-center md:space-y-0 justify-center mb-10">
+              <div>
+                <Link
+                  className=" uppercase text-white bg-pyconug-lightBlue border-2 hover:bg-white hover:text-pyconug-lightBlue hover:border-pyconug-lightBlue px-12 py-4 rounded-md w-64 font-bold "
+                  href={"/keynote-speakers"}
+                >
+                  View Keynote speakers
+                </Link>
+              </div>
+            </div>
+          </>
+        )}
       </section>
     </>
   );
