6f58ab527cf7f11077a72c8564651770656b7c66
HassanBahati <mukisabahati@gmail.com>
2024-05-01 17:30:38 +0300

    feat: add speakers component

--- app/2024/speakers/speaker-guide.jsx
+++ app/2024/speakers/speaker-guide.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function SpeakersPage() {
+export default function SpeakersGuide() {
   return (
     <>
       {" "}
---
+++ app/2024/speakers/speakers.jsx
@@ -0,0 +1,19 @@
+export default function Speakers() {
+  return (
+    <>
+      {" "}
+      <section className="flex bg-cyan-900 text-zinc-300">
+        <div className="flex py-10 lg:items-center">
+          <div className="md:mx-10 text-center mx-4">
+            <h1 className="text-3xl font-extrabold sm:text-5xl text-white">
+              Speakers
+            </h1>
+            <p className="mt-4 sm:text-xl text-justify mx-5">
+              We shall be releasing the list of speakers soon.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
