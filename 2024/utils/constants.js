36a30ae4a734e3284b51af01df0e6f7894abf73c
Hassan Bahati <65954740+HassanBahati@users.noreply.github.com>
2024-05-01 17:52:43 +0300

    close CFP 2024 (#124)
    Closes #125

--- 2024/utils/constants.js
+++ 2024/utils/constants.js
@@ -2,3 +2,13 @@ export const VENUE = `NWSC International Resource Centre (IREC)`;
 export const CONFERENCE_DATE = `9th - 13th October, 2024`;
 export const CFP_LINK = `https://www.papercall.io/pyconug`;
 export const DIVERSITY_FINANCIAL_AID_LINK = `https://bit.ly/pyconUG-2024-diversity-aid`;
+
+export const SPEAKERS_2024 = [
+  // {
+  //   name: "",
+  //   image: "/assets/images/speakers/",
+  //   description:
+  //     "",
+  //   bio: "",
+  // },
+];
