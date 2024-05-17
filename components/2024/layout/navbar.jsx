230bc3d7775ba519466db68fda3bc48cbddff070
GeoXhacker <mukisageophrey@gmail.com>
2024-05-17 11:34:43 +0300

    added mobile navigation menu

--- components/2024/layout/navbar.jsx
+++ components/2024/layout/navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Dialog, Disclosure } from "@headlessui/react";
+import { Cross1Icon, HamburgerMenuIcon, ChevronDownIcon} from "@radix-ui/react-icons"
 import Logo from "@/components/ui/logo";
 const NavbarLinks = [
     // {
@@ -108,6 +109,65 @@ export default function Example() {
                     </button>
                 </div>
             </nav>
+            <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                <div className="fixed inset-0 z-10" />
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <Logo />
+                        <button
+                            type="button"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(false)}
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <Cross1Icon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                {NavbarLinks.map((link, i) => (
+                                    link?.subLinks?.length ? (
+                                        <Disclosure as="div" className="-mx-3" key={i}>
+                                            {({ open }) => (
+                                                <>
+                                                    <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50">
+                                                        {link.label}
+                                                        <ChevronDownIcon
+                                                            className={`${open ? 'rotate-180' : ''} h-5 w-5 flex-none`}
+                                                            aria-hidden="true"
+                                                        />
+                                                    </Disclosure.Button>
+                                                    <Disclosure.Panel className="mt-2 space-y-2">
+                                                        {link.subLinks.map((subLink, j) => (
+                                                            <Disclosure.Button
+                                                                key={j}
+                                                                as="a"
+                                                                href={subLink.path}
+                                                                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                                            >
+                                                                {subLink.label}
+                                                            </Disclosure.Button>
+                                                        ))}
+                                                    </Disclosure.Panel>
+                                                </>
+                                            )}
+                                        </Disclosure>
+                                    ) : (
+                                        <a
+                                            href={link.path}
+                                            key={i}
+                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                        >
+                                            {link.label}
+                                        </a>
+                                    )
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </Dialog.Panel>
+            </Dialog>
         </header>
     );
 }
