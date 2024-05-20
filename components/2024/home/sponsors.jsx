e905d35216884a879cc106b0f4ea741a2a3b5c69
HassanBahati <mukisabahati@gmail.com>
2024-05-20 17:07:47 +0300

    feat: add jetbrains to 2024 sponsors

--- components/2024/home/sponsors.jsx
+++ components/2024/home/sponsors.jsx
@@ -10,14 +10,23 @@ export default function Sponsors() {
             Our Sponsors
           </h2>
           <p className="text-base p-4 font-medium">
-            Join our growing list of sponsors who are making PyCon Uganda happen
+            Join our growing list of sponsors who are making PyCon Uganda
+            happen.
           </p>
           <p className="text-xl font-bold p-4">Expo Sponsors</p>
 
-          <Link href="https://github.com" target="_blank">
-            <Image src="/2024/sponsors/GitHub.png" width={500} height={500} />
-          </Link>
-
+          <div className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-8 my-8">
+            <Link href="https://github.com" target="_blank">
+              <Image src="/2024/sponsors/GitHub.png" width={500} height={500} />
+            </Link>
+            <Link href="https://www.jetbrains.com" target="_blank">
+              <Image
+                src="https://resources.jetbrains.com/storage/products/company/brand/logos/jetbrains.png"
+                width={500}
+                height={500}
+              />
+            </Link>
+          </div>
           <div className="py-12">
             <Link
               className=" uppercase text-white bg-pyconug-lightBlue border-2 hover:bg-white hover:text-pyconug-lightBlue hover:border-pyconug-lightBlue px-12 py-4 rounded-md w-64 font-bold "
