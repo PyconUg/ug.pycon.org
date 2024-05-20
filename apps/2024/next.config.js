4ab3c73e2d577ed4f1d57f5ac1a9addd4333496c
amkayondo01@gmail.com <amkayondo01@gmail.com>
2024-05-21 00:06:59 +0300

    chore: Fix rewrites bug in next.config.js
    This commit fixes a bug in the `next.config.js` file related to rewrites. The rewrites for the `/2024` route were missing, causing incorrect redirection. This commit adds the missing rewrites for the `/2024` route, ensuring proper redirection.

--- apps/2024/next.config.js
+++ apps/2024/next.config.js
@@ -8,26 +8,32 @@ module.exports = {
     ignoreBuildErrors: true,
   },
   // fix the rewrites bug
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/2023/:path*",
-  //       destination: `${process.env.NODE_ENV
-  //         === "development" ? "http://localhost:3002" : "https://ug.pycon.org"
-  //       }/:path*`,
-  //     },
-  //     {
-  //       source: "/2023",
-  //       destination: `${process.env.NODE_ENV
-  //         === "development" ? "http://localhost:3002" : "https://ug.pycon.org"
-  //       }/:path*`,
-  //     },
-  //     {
-  //       source: "/:path*",
-  //       destination: `${process.env.NODE_ENV
-  //         === "development" ? "http://localhost:3001" : "https://ug.pycon.org"
-  //       }/:path*`,
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/2023/:path*",
+        destination: `${process.env.NODE_ENV
+          === "development" ? "http://localhost:3002" : "https://ug.pycon.org"
+        }/:path*`,
+      },
+      {
+        source: "/2023",
+        destination: `${process.env.NODE_ENV
+          === "development" ? "http://localhost:3002" : "https://ug.pycon.org"
+        }/:path*`,
+      },
+      {
+        source: "/2024/:path*",
+        destination: `${process.env.NODE_ENV
+          === "development" ? "http://localhost:3001" : "https://ug.pycon.org"
+        }/:path*`,
+      },
+      {
+        source: "/:path*",
+        destination: `${process.env.NODE_ENV
+          === "development" ? "http://localhost:3001" : "https://ug.pycon.org"
+        }/:path*`,
+      },
+    ];
+  },
 };
