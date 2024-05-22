ede02a047334ac4755f9c0e9b5828c7754103dd2
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-05-22 15:16:55 +0300

    adding chaoss Africa as sponsor

--- components/2024/home/sponsors.jsx
+++ components/2024/home/sponsors.jsx
@@ -15,13 +15,20 @@ export default function Sponsors() {
           </p>
           <p className="text-xl font-bold p-4">Expo Sponsors</p>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-8 my-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 place-items-center gap-8 my-8">
             <Link href="https://github.com" target="_blank">
               <Image src="/2024/sponsors/GitHub.png" width={500} height={500} />
             </Link>
             <Link href="https://www.jetbrains.com" target="_blank">
               <Image
                 src="https://resources.jetbrains.com/storage/products/company/brand/logos/jetbrains.png"
+                width={500}
+                height={500}
+              />
+            </Link>
+            <Link href="https://chaoss.community/" target="_blank">
+              <Image
+                src="/2024/sponsors/CHAOSS AFRICA LOGO.png"
                 width={500}
                 height={500}
               />
