c8714a94247ba0c4460fc728ec5dfcd0b3a0bbfc
Aaron Leonard Oluk <57962908+Leon12-aaryson@users.noreply.github.com>
2024-05-22 15:43:07 +0300

    add Chaoss to 2024 sponsors (#142)
    - adding Choass africa as sponsor

--- app/2024/sponsors/page.jsx
+++ app/2024/sponsors/page.jsx
@@ -20,13 +20,20 @@ export default function SponsorsPage() {
       <section className="lg:mx-28 mx-8 min-h-[55vh]">
         <p className="text-4xl font-bold my-8 text-gray-500">Expo Sponsors</p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 place-items-center gap-8">
           <Link href="https://github.com" target="_blank">
             <Image src="/2024/sponsors/GitHub.png" width={500} height={500} />
           </Link>
           <Link href="https://www.jetbrains.com" target="_blank">
             <Image
               src="https://resources.jetbrains.com/storage/products/company/brand/logos/jetbrains.png"
+              width={500}
+              height={500}
+            />
+          </Link>
+          <Link href="https://chaoss.community/" target="_blank">
+            <Image
+              src="/2024/sponsors/CHAOSS AFRICA LOGO.png"
               width={500}
               height={500}
             />
