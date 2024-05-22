aa0958ee3762b626957ea86ec36a2d4d83fb358d
Aaron Leonard Oluk <57962908+Leon12-aaryson@users.noreply.github.com>
2024-05-22 09:28:16 +0300

    feat: add django girls to PyCon 2024 Communities (#140)
    - changed the Django girls link to the current on(2024)
    
    ---------
    
    Co-authored-by: Hassan Bahati <65954740+HassanBahati@users.noreply.github.com>

--- components/2024/layout/navbar.jsx
+++ components/2024/layout/navbar.jsx
@@ -139,16 +139,22 @@ const NavbarLinks = [
       // },
     ],
   },
-  // {
-  //   path: "#",
-  //   label: "Communities",
-  //   subLinks: [
-  //     {
-  //       path: "/2024/django-girls",
-  //       label: "Django Girls",
-  //     },
-  //   ],
-  // },
+  {
+    path: "#",
+    label: "Communities",
+    subLinks: [
+      {
+        path: "/2024/django-girls",
+        label: "Django Girls",
+        description: "Empowering women! Join us at PyCon Uganda 2024 for Django workshops.",
+        icon: (
+          <UserGroupIcon
+            className="h-6 w-6 text-gray-600 group-hover:text-pyconug-lightBlue"
+            aria-hidden="true"
+          />),
+      },
+    ],
+  },
 ];
 
 function classNames(...classes) {
