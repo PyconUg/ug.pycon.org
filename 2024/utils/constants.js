dac2619e771326ff37fe8e45f563220819f7ffdf
HassanBahati <mukisabahati@gmail.com>
2024-06-04 13:05:24 +0300

    chore: update prospectus

--- 2024/utils/constants.js
+++ 2024/utils/constants.js
@@ -2,7 +2,7 @@ export const VENUE = `NWSC International Resource Centre (IREC)`;
 export const CONFERENCE_DATE = `9th - 13th October, 2024`;
 export const CFP_LINK = `https://www.papercall.io/pyconug`;
 export const DIVERSITY_FINANCIAL_AID_LINK = `https://bit.ly/pyconUG-2024-diversity-aid`;
-export const PYCONUGANDA_PROSPECTUS_2024 = `https://firebasestorage.googleapis.com/v0/b/pycon-uganda.appspot.com/o/PyCon%20Uganda%20Prospectus%20(1).pdf?alt=media&token=45c29c52-75f7-4174-8199-aa3621f3d987`;
+export const PYCONUGANDA_PROSPECTUS_2024 = `https://firebasestorage.googleapis.com/v0/b/pycon-uganda.appspot.com/o/PyCon-Uganda-2024-Prospectus.pdf?alt=media&token=052e8898-4041-4a46-a564-d45a3ce822e7`;
 export const DIVERSITY_TRAVEL_AID_LINK_2024 = `https://forms.gle/kgYRfgFHQxdmB61A9`;
 export const DJANGO_GIRLS_REG = "https://forms.gle/ZwTRAcYzRGXjskiN7";
 
