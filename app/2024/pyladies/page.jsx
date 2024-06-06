4238c9931ec3954cdef8d84083660576b235e952
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-06-06 21:46:04 +0300

    resolve the error of use of quotes

--- app/2024/pyladies/page.jsx
+++ app/2024/pyladies/page.jsx
@@ -27,7 +27,7 @@ export default function DjangoGirls() {
         </div>
       </section> */}
       <section className="my-10 justify-center items-center text-justify text-lg">
-        <div class="mx-8 md:mx-28 max-w-screen-xl flex flex-col justify-center items-center mb-10">
+        <div className="mx-8 md:mx-28 max-w-screen-xl flex flex-col justify-center items-center mb-10">
           <Image
             width={150}
             height={75}
@@ -47,16 +47,16 @@ export default function DjangoGirls() {
           </div>
           <div className="lg:w-1/2 p-4 w-full">
             <h1 className="text-3xl">Join Our Anniversary Workshop!</h1>
-            <p className="mt-3">Don't miss this chance to start your Python journey and join a supportive
+            <p className="mt-3">Don&pos;t miss this chance to start your Python journey and join a supportive
               community. Apply now for the PyLadies Anniversary Workshop!</p>
-            <p className="mt-3">As a workshop attendee, you'll have the opportunity to:</p>
+            <p className="mt-3">As a workshop attendee, you&apos;ll have the opportunity to:</p>
             <ul className="list-disc list-inside indent-4">
               <li>Learn Python Basics: Start writing your first Python programs
                 with guidance from experienced mentors.</li>
               <li>Network and Connect: Meet and connect with fellow women in tech and share your experiences.</li>
               <li>Engage with Experts: Interact with organizers and sponsors to enhance your learning journey.</li>
             </ul>
-            <p className="mt-3 font-medium">Don't miss this fantastic opportunity to kickstart your Python journey and
+            <p className="mt-3 font-medium">Don&apos;t miss this fantastic opportunity to kickstart your Python journey and
               become part of a supportive community. Apply now to join us at the PyLadies Anniversary Workshop!</p>
           </div>
         </div>
@@ -66,7 +66,7 @@ export default function DjangoGirls() {
           <div className="mx-8 md:mx-28 max-w-screen-xl flex flex-col lg:flex-row mb-10">
             <div className="lg:w-full p-4">
               <h1 className="text-3xl">Become a participant</h1>
-              <p className="mt-3">Feel free to apply and spread the word about this workshop. Let's dream, code, and create together!</p>
+              <p className="mt-3">Feel free to apply and spread the word about this workshop. Let&apos;s dream, code, and create together!</p>
               <div className="flex justify-center md:justify-start">
                 <Link
                   className="bg-white text-pyconug-lightBlue px-12 py-4 rounded-md w-50 mt-10"
@@ -82,17 +82,17 @@ export default function DjangoGirls() {
         <div className="mx-8 md:mx-28 max-w-screen-xl flex flex-col lg:flex-row mb-10">
           <div className="lg:w-1/2 p-4 w-full">
             <h3 className="text-xl font-bold">Do I need to know anything about websites or programming?</h3>
-            <p className="mt-3">No! This workshop is for beginners. You don't need any prior knowledge. If you have some technical knowledge (like knowing what HTML or CSS are), you can still apply!</p>
+            <p className="mt-3">No! This workshop is for beginners. You don&apos;t need any prior knowledge. If you have some technical knowledge (like knowing what HTML or CSS are), you can still apply!</p>
             <h3 className="text-xl font-bold mt-5">Should I bring my own laptop?</h3>
-            <p className="mt-3">Yes, please bring your own laptop. We don't provide hardware, and it's important for you to have your
+            <p className="mt-3">Yes, please bring your own laptop. We don&apos;t provide hardware, and it&apos;s important for you to have your
               computer to take home everything you create during the workshop.</p>
           </div>
           <div className="lg:w-1/2 p-4 w-full">
             <h3 className="text-xl font-bold">Do I need to install anything on my laptop beforehand?</h3>
-            <p className="mt-3">It would be helpful to have Python installed, but don't worry if you haven't done it yet.
-              We'll have an installation session before the workshop to help you get set up.</p>
+            <p className="mt-3">It would be helpful to have Python installed, but don&apos;t worry if you haven&apos;t done it yet.
+              We&apos;ll have an installation session before the workshop to help you get set up.</p>
             <h3 className="text-xl font-bold mt-5">Do I need to be a PyLady already to attend?</h3>
-            <p className="mt-3">No. Simply sign up for the workshop session, and you're welcome to join other activities if you're interested.</p>
+            <p className="mt-3">No. Simply sign up for the workshop session, and you&apos;re welcome to join other activities if you&apos;re interested.</p>
           </div>
         </div>
         <div class="flex flex-col items-center space-x-4">
