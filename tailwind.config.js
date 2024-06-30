b73ad68502350fa612ce99157afdc99f90463023
HassanBahati <mukisabahati@gmail.com>
2024-06-30 15:06:23 +0300

    style: update talind paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./2023/**/*.{js,ts,jsx,tsx}",
+    "./2024/**/*.{js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
