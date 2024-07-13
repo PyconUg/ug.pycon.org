23989fb78d3ac5771ce26b9940e5a0b50a21f1fb
GeoXhacker <mukisageophrey@gmail.com>
2024-07-13 16:16:15 +0300

    removed bio key

--- components/speakers/keynote-speakers-data.js
+++ components/speakers/keynote-speakers-data.js
@@ -6,7 +6,6 @@ export const KEYNOTESPEAKERS = [
       role: "Core Python Developer | Jupyter Core Contributor",
       twitter: "https://x.com/WillingCarol",
       linkedIn: "https://www.linkedin.com/in/carolwilling",
-      bio: "Python, Cloud and OSS at Microsoft. Author of CPython Internals. x3 Father, Christian, PSF Fellow, Fellow at Macquarie University strive to be an active, engaged and responsible member of both my local community and the broader open-source tech community. I work for Microsoft, focused on Python and Open-Sources technologies. I Tweet often, blog frequently and speak seldomly. All of my research and tinkering is Open-Source, I'm an active member of some large open-source projects as well as maintaining my own."
     },
     {
       name: "Łukasz Langa",
@@ -15,7 +14,6 @@ export const KEYNOTESPEAKERS = [
       role: "CPython Developer in Residence",
       twitter: "https://x.com/llanga",
       linkedIn: "https://www.linkedin.com/in/llanga",
-      bio: "Speaker | Experienced Field Officer with a demonstrated history of working in the computer software industry. Strong community and social services professional. Monica Oyugi is a software developer and digital marketer, a speaker, with a history of working in the computer software industry. I love writing code because I have always been obsessed with the idea that they solve practical problems. I enjoy live bands and I prefer meeting people physically over social media.",
     },
     {
       name: "Marlene Mhangami",
@@ -24,7 +22,6 @@ export const KEYNOTESPEAKERS = [
       image: "/assets/images/keynote-speakers/marlene.jpeg",
       twitter: "https://x.com/marlene_zw",
       linkedIn: "https://www.linkedin.com/in/marlenemhangami",
-      bio: "Speaker at Django or Python events or conferences world wide, DjangoCon US organizer(since 2016), former DEFNA board member and current DEFNA North American Ambassador. - Data analysis, research and problem-solving abilities enhanced via ongoing self-taught software engineering skills - Communication and presentations skills refined as an instructor & TA for university-level accounting, finance & computing courses and by presenting at technical conferences - Broad accounting/financial analysis experience including budget & variance analysis, financial auditing, monthly closing, and financial statement preparation",
     },
   ];
   
