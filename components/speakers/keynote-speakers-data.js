a72d1f35101dbd76599c180b543fe81a32621312
GeoXhacker <mukisageophrey@gmail.com>
2024-07-13 15:56:52 +0300

    updated keynotes list

--- components/speakers/keynote-speakers-data.js
+++ components/speakers/keynote-speakers-data.js
@@ -1,27 +1,24 @@
 export const KEYNOTESPEAKERS = [
     {
-      name: "Anthony Shaw",
-      image: "/assets/images/keynote-speakers/anthony-shaw.jpeg",
-      description: "Python and Open Source Advocate at Microsoft",
+      name: "Carol Willing",
+      image: "/assets/images/keynote-speakers/carol.jpeg",
+      description: "Python Steer Council | Python Core Developer | PSF Fellow | Jupyter Core Contributor",
+      role: "Core Python Developer | Jupyter Core Contributor",
       bio: "Python, Cloud and OSS at Microsoft. Author of CPython Internals. x3 Father, Christian, PSF Fellow, Fellow at Macquarie University strive to be an active, engaged and responsible member of both my local community and the broader open-source tech community. I work for Microsoft, focused on Python and Open-Sources technologies. I Tweet often, blog frequently and speak seldomly. All of my research and tinkering is Open-Source, I'm an active member of some large open-source projects as well as maintaining my own."
     },
     {
-      name: "Monica Oyugi",
-      image: "/assets/images/keynote-speakers/monica-oyugi.jpg",
-      description: "Co-Organizer at apifestivalke, pythonairobi | Digital Marketer at droidconke | WomanInTech | devTraveler | poetry",
+      name: "Łukasz Langa",
+      image: "/assets/images/keynote-speakers/langa.jpeg",
+      description: "CPython Developer in Residence | Python 3.8 & 3.9 | Original Creator Of Black | Pianist | Dad",
+      role: "CPython Developer in Residence",
       bio: "Speaker | Experienced Field Officer with a demonstrated history of working in the computer software industry. Strong community and social services professional. Monica Oyugi is a software developer and digital marketer, a speaker, with a history of working in the computer software industry. I love writing code because I have always been obsessed with the idea that they solve practical problems. I enjoy live bands and I prefer meeting people physically over social media.",
     },
     {
-      name: "Kojo Idrissa",
-      description: "Software engineer with an accounting degree and an MBA",
-      image: "/assets/images/keynote-speakers/kojo-idrissa.jpeg",
+      name: "Marlene Mhangami",
+      description: "Developer Advocate | Software Engineer at Microsoft",
+      role: "Software Engineer at Microsoft",
+      image: "/assets/images/keynote-speakers/marlene.jpeg",
       bio: "Speaker at Django or Python events or conferences world wide, DjangoCon US organizer(since 2016), former DEFNA board member and current DEFNA North American Ambassador. - Data analysis, research and problem-solving abilities enhanced via ongoing self-taught software engineering skills - Communication and presentations skills refined as an instructor & TA for university-level accounting, finance & computing courses and by presenting at technical conferences - Broad accounting/financial analysis experience including budget & variance analysis, financial auditing, monthly closing, and financial statement preparation",
-    },
-    {
-      name: "Stephen Musoke Senkomago",
-      image: "/assets/images/keynote-speakers/ss-musoke.jpg",
-      description: "Ramblings of a father, husband, tech lover, amateur poet, book lover, who believes family makes him and culture holds all together",
-      bio: "I am a technologist at heart now building a mobile web interface and mobile Android app to bring the Beyonic business payments and collections suite to a wide audience of SMEs across Africa. Previously I was in health technology space, leading the architecture, design, development and rollout of a national Electronic Medical Records system for Uganda, UgandaEMR, based on OpenMRS, as well as supporting the building of national disease surveillance infrastructure, intergrating data from different sources into a single coherent warehouse. I leverage my entrepreneurial experiences to drive the adoption of technology to achieve business goals whether bringing new products & services to the market, improving delivery of their existing portfolios or reaching out to new unknown areas. I am passionate about growing the technology community in Uganda and on the Africa continent through “Coded in Africa, for Africans by Africans” through use of opensource technologies. A personal goal in this journey is working towards having women as equal participants in the African technology revolution.",
     },
   ];
   
