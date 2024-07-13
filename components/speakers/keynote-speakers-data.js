4b79bf1a89bdb77b02b27cc9e6d91b1afe33760a
GeoXhacker <mukisageophrey@gmail.com>
2024-07-13 16:15:30 +0300

    added social media links

--- components/speakers/keynote-speakers-data.js
+++ components/speakers/keynote-speakers-data.js
@@ -4,6 +4,8 @@ export const KEYNOTESPEAKERS = [
       image: "/assets/images/keynote-speakers/carol.jpeg",
       description: "Python Steer Council | Python Core Developer | PSF Fellow | Jupyter Core Contributor",
       role: "Core Python Developer | Jupyter Core Contributor",
+      twitter: "https://x.com/WillingCarol",
+      linkedIn: "https://www.linkedin.com/in/carolwilling",
       bio: "Python, Cloud and OSS at Microsoft. Author of CPython Internals. x3 Father, Christian, PSF Fellow, Fellow at Macquarie University strive to be an active, engaged and responsible member of both my local community and the broader open-source tech community. I work for Microsoft, focused on Python and Open-Sources technologies. I Tweet often, blog frequently and speak seldomly. All of my research and tinkering is Open-Source, I'm an active member of some large open-source projects as well as maintaining my own."
     },
     {
@@ -11,6 +13,8 @@ export const KEYNOTESPEAKERS = [
       image: "/assets/images/keynote-speakers/langa.jpeg",
       description: "CPython Developer in Residence | Python 3.8 & 3.9 | Original Creator Of Black | Pianist | Dad",
       role: "CPython Developer in Residence",
+      twitter: "https://x.com/llanga",
+      linkedIn: "https://www.linkedin.com/in/llanga",
       bio: "Speaker | Experienced Field Officer with a demonstrated history of working in the computer software industry. Strong community and social services professional. Monica Oyugi is a software developer and digital marketer, a speaker, with a history of working in the computer software industry. I love writing code because I have always been obsessed with the idea that they solve practical problems. I enjoy live bands and I prefer meeting people physically over social media.",
     },
     {
@@ -18,6 +22,8 @@ export const KEYNOTESPEAKERS = [
       description: "Developer Advocate | Software Engineer at Microsoft",
       role: "Software Engineer at Microsoft",
       image: "/assets/images/keynote-speakers/marlene.jpeg",
+      twitter: "https://x.com/marlene_zw",
+      linkedIn: "https://www.linkedin.com/in/marlenemhangami",
       bio: "Speaker at Django or Python events or conferences world wide, DjangoCon US organizer(since 2016), former DEFNA board member and current DEFNA North American Ambassador. - Data analysis, research and problem-solving abilities enhanced via ongoing self-taught software engineering skills - Communication and presentations skills refined as an instructor & TA for university-level accounting, finance & computing courses and by presenting at technical conferences - Broad accounting/financial analysis experience including budget & variance analysis, financial auditing, monthly closing, and financial statement preparation",
     },
   ];
