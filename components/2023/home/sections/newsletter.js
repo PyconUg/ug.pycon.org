adf1c29723f72e193987cda1286df16de8322b35
HassanBahati <mukisabahati@gmail.com>
2024-07-15 21:09:37 +0300

    style: adjusted newsletter section vertical padding

--- components/2023/home/sections/newsletter.js
+++ components/2023/home/sections/newsletter.js
@@ -21,7 +21,7 @@ export default function Newsletter() {
   return (
     <section>
       <div className="">
-        <div className="mx-auto max-w-screen-xl px-4 py-16 lg:flex lg:h-60 lg:items-center">
+        <div className="mx-auto max-w-screen-xl px-4 py-14 lg:flex lg:items-center">
           <div className="mx-auto max-w-xl text-center">
             <h2 className="text-4xl text-[#0e1b4d] font-bold uppercase">
               Newsletter
