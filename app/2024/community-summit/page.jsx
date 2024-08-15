8778654f6f61cd58c4c242595309534bbd3adc91
HassanBahati <mukisabahati@gmail.com>
2024-08-15 12:13:49 +0300

    feat: add speaker images to community schedule

--- app/2024/community-summit/page.jsx
+++ app/2024/community-summit/page.jsx
@@ -1,7 +1,62 @@
+import ScheduleStep from "@/2024/schedule/schedule-step";
+import Image from "next/image";
 import Link from "next/link";
 
-
 export default function CommunitySummit() {
+  const PYCON_DAY1_SCHEDULE = [
+    {
+      title: "Welcome remarks and Introductions",
+      duration: "10:00 - 1:00",
+      speakerAvatar: "",
+      speaker: "Sumaiya Nalukwago",
+    },
+    {
+      title: "Keynote Session",
+      duration: "11:00 - 12:00",
+      speaker: "Sumaiya Nalukwago",
+      speakerAvatar: "",
+    },
+    {
+      title:
+        "From Workshops to Masterminds: Building a thriving Python Community with the Python Ho User Group.",
+      duration: "12:00 - 12:30",
+      speakerAvatar: "/2024/speakers/KAFUI ALORDO.png",
+      speaker: "Kafui Alordo",
+    },
+    {
+      title:
+        "Empowering Diverse Communities with Python: Insights from Mombasa, Kenya.",
+      duration: "12:00 - 12:30",
+      speakerAvatar: "/2024/speakers/Chris N. Achinga.png",
+      speaker: "Chris N. Achinga",
+    },
+    {
+      title: "Lunch break",
+      duration: "13:00 - 14:00",
+    },
+    {
+      title:
+        "Harnessing Collective Intelligence: Developing and Utilizing Data Science Communities",
+      duration: "14:00 - 14:30",
+      speaker: "Bernice Omiunu",
+    },
+    {
+      title: "OpenSource at Pykampala community",
+      duration: "14:30 - 15:00",
+      speakerAvatar: "",
+      speaker: "Tobias Higenyi",
+    },
+    {
+      title: "Discussion on Community way forward",
+      duration: "15:00 - 16:00",
+      speakerAvatar: "",
+      speaker: "Sumaiya Nalukwago",
+    },
+    {
+      title: "Closing Remarks and End",
+      duration: "16:00 - 16:30",
+    },
+  ];
   return (
     <>
       <section className="flex bg-cyan-900 text-zinc-300 justify-center">
@@ -20,197 +75,33 @@ export default function CommunitySummit() {
         </div>
       </section>
 
-
       <section className="mx-8 md:mx-28 max-w-screen-xl my-10 text-justify text-lg">
         <div class="container mx-auto">
           <h2 className="text-3xl font-bold sm:text-5xl text-black">
             About Python Community Summit
           </h2>
           <p className="mt-4 sm:text-xl mx-5">
-            The Python Community Summit is an annual event that brings together Python enthusiasts from around the world. It provides a platform for community members to connect, share knowledge, and collaborate on various Python-related topics. The summit features keynote sessions, workshops, panel discussions, and networking opportunities. Whether you are a beginner or an experienced Python developer, the Python Community Summit offers something for everyone. Join us and be a part of this vibrant and inclusive community!
+            The Python Community Summit is an annual event that brings together
+            Python enthusiasts from around the world. It provides a platform for
+            community members to connect, share knowledge, and collaborate on
+            various Python-related topics. The summit features keynote sessions,
+            workshops, panel discussions, and networking opportunities. Whether
+            you are a beginner or an experienced Python developer, the Python
+            Community Summit offers something for everyone. Join us and be a
+            part of this vibrant and inclusive community!
           </p>
         </div>
       </section>
 
-
-
-      <section className="mx-8 md:mx-28 max-w-screen-xl my-10 text-justify text-lg">
-        <div class="container mx-auto">
-          <table class="border-separate w-full mt-4" style={{ borderSpacing: '0.5rem' }}>
-            <thead>
-              <tr>
-                <th class="p-4 bg-white rounded shadow">Time</th>
-                <th class="p-4 bg-white rounded shadow">Activity</th>
-                <th class="p-4 bg-white rounded shadow">Facilitator</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="rounded shadow bg-cyan-700">
-                  <div class="rounded text-center text-white p-2">
-                    <h4>10:00 to 11:00</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Welcome remarks and Introductions</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Sumaiya Nalukwago</h4>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="rounded shadow bg-cyan-700">
-                  <div class="rounded text-center text-white p-2">
-                    <h4>11:00 to 12:00</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Keynote Session</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Sumaiya Nalukwago</h4>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="rounded shadow bg-cyan-700">
-                  <div class="rounded text-center text-white p-2">
-                    <h4>12:00 to 12:30</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>From Workshops to Masterminds: Building a thriving Python Community with the Python Ho User Group.</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Kafui Alordo</h4>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="rounded shadow bg-cyan-700">
-                  <div class="rounded text-center text-white p-2">
-                    <h4>12:30 to 13:00</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Empowering Diverse Communities with Python: Insights from Mombasa, Kenya.</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Chris N. Achinga</h4>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="rounded shadow bg-cyan-700">
-                  <div class="rounded text-center text-white p-2">
-                    <h4>13:00 to 14:00</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-gray-500 text-white" colSpan="2">
-                  <div class="p-2 font-bold text-base">
-                    <h4>Lunch break</h4>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="rounded shadow bg-cyan-700">
-                  <div class="rounded text-center text-white p-2">
-                    <h4>14:00 to 14:30</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Harnessing Collective Intelligence: Developing and Utilizing Data Science Communities</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Bernice Omiunu</h4>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="rounded shadow bg-cyan-700">
-                  <div class="rounded text-center text-white p-2">
-                    <h4>14:30 to 15:00</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>OpenSource at Pykampala community</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Tobias Higenyi</h4>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="rounded shadow bg-cyan-700">
-                  <div class="rounded text-center text-white p-2">
-                    <h4>15:00 to 15:30</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Discussion on Community way forward</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Sumaiya Nalukwago</h4>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="rounded shadow bg-cyan-700">
-                  <div class="rounded text-center text-white p-2">
-                    <h4>15:30 to 16:00</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Discussion on Community way forward</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white">
-                  <div class="p-2 text-black">
-                    <h4>Sumaiya Nalukwago</h4>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="rounded shadow bg-cyan-700">
-                  <div class="rounded text-center text-white p-2">
-                    <h4>16:00 to 16:30</h4>
-                  </div>
-                </td>
-                <td class="rounded shadow bg-white" colSpan={'2'}>
-                  <div class="p-2 text-black">
-                    <h4>Closing Remarks and End</h4>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-
-      </section >
+      <section className="mx-8 md:mx-28 max-w-screen-xl ">
+        <section className=" ">
+          <ol className="relative border-l border-gray-200 dark:border-gray-700 dark:text-gray-400 ">
+            {PYCON_DAY1_SCHEDULE?.map((step, index) => (
+              <ScheduleStep key={index} stepData={step} />
+            ))}
+          </ol>
+        </section>
+      </section>
     </>
   );
 }
