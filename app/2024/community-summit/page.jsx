191717a085022a9d4075339bf5bdf2868c582ea7
HassanBahati <mukisabahati@gmail.com>
2024-08-16 09:07:07 +0300

    chore: add community summit speaker photo

--- app/2024/community-summit/page.jsx
+++ app/2024/community-summit/page.jsx
@@ -7,14 +7,14 @@ export default function CommunitySummit() {
     {
       title: "Welcome remarks and Introductions",
       duration: "10:00 - 1:00",
-      speakerAvatar: "",
+      speakerAvatar: "/2024/speakers/Sumaiya Nalukwago.jpeg",
       speaker: "Sumaiya Nalukwago",
     },
     {
       title: "Keynote Session",
       duration: "11:00 - 12:00",
       speaker: "Sumaiya Nalukwago",
-      speakerAvatar: "",
+      speakerAvatar: "/2024/speakers/Sumaiya Nalukwago.jpeg",
     },
     {
       title:
@@ -50,7 +50,7 @@ export default function CommunitySummit() {
     {
       title: "Discussion on Community way forward",
       duration: "15:00 - 16:00",
-      speakerAvatar: "",
+      speakerAvatar: "/2024/speakers/Sumaiya Nalukwago.jpeg",
       speaker: "Sumaiya Nalukwago",
     },
     {
