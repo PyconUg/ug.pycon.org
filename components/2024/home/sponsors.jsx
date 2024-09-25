bb6d7e0b23b259d54216b97bc5695431029db24e
amkayondo01@gmail.com <amkayondo01@gmail.com>
2024-09-26 00:16:43 +0300

    Update PyCon Uganda 2024 website content and structure
    - Add Community Summit schedule and details
    - Update contact information for Code of Conduct
    - Implement new banner announcing PyCon Africa 2026 hosting
    - Refactor About section for 2024 conference
    - Add PyData conference page and registration link
    - Update jumbotron component with 2024 conference details
    - Implement speaker guide page with submission instructions
    - Update dependencies and project configuration

--- components/2024/home/sponsors.jsx
+++ components/2024/home/sponsors.jsx
@@ -5,35 +5,74 @@ import { SPONSORS_2024 } from "@/2024/utils/constants";
 
 export default function Sponsors() {
   return (
-    <section className=" bg-[#f6f7fd]">
-      <div className="mx-auto max-w-screen-xl px-4 lg:flex  lg:items-center">
-        <div className="mx-auto max-w-xl text-center">
-          <h2 className="text-4xl text-[#0e1b4d] font-bold uppercase">
-            Our Sponsors
-          </h2>
-          <p className="text-base p-4 font-medium">
-            Join our growing list of sponsors who are making PyCon Uganda
-            happen.
-          </p>
-
-          <div>
-            {Object.entries(SPONSORS_2024).map(([key, sponsors]) => (
-              <SponsorList
-                key={key}
-                title={key.charAt(0).toUpperCase() + key.slice(1)}
-                sponsors={sponsors}
-              />
-            ))}
-          </div>
-          <div className="py-12">
-            <Link
-              className=" uppercase text-white bg-pyconug-lightBlue border-2 hover:bg-white hover:text-pyconug-lightBlue hover:border-pyconug-lightBlue px-12 py-4 rounded-md w-64 font-bold "
-              href={"/2024/sponsors/why-sponsor"}
-            >
-              Become a Sponsor
-            </Link>
+    <section className="bg-[#f6f7fd] relative">
+      <div className="mx-auto max-w-container px-4 relative">
+        <div className="grid lg:grid-cols-12 md:grid-cols-12 grid-cols-1 gap-2 lg:gap-6">
+          <div className="lg:col-span-12 md:col-span-12">
+            <span className="h-16 lg:h-32 block"></span>
+            <div className="grid lg:grid-cols-12 md:grid-cols-12 grid-cols-[100%] gap-2 lg:gap-6">
+              <div className="lg:col-span-5 md:col-span-5">
+                <div className="sticky top-48">
+                  <h1 className="font-sans font-bold text-4xl leading-10 lg:text-5xl lg:leading-16 text-black text-center md:text-left">
+                    Sponsor us!
+                  </h1>
+                  <span className="h-8 lg:h-12 block"></span>
+                  <p className="font-sans break-words transition-colors font-medium whitespace-pre-wrap select-auto text-md leading-7 lg:text-2md lg:leading-8 text-grey-900 text-center md:text-left">
+                    With over 200+ attendees expected, this is your chance to
+                    boost brand visibility, connect with top Python
+                    professionals, generate leads, and showcase your commitment
+                    to the Python community. Encourage your company to sponsor
+                    and be a part of this dynamic event!
+                  </p>
+                  <span className="h-8 lg:h-12 block"></span>
+                  <Link
+                    className="inline-flex items-center uppercase select-none  border cursor-pointer transition-colors hover:bg-[#1a9baa] bg-[#177e89] border-[#177e89] py-5 px-8 justify-center w-full md:w-auto"
+                    href="/2024/sponsors/why-sponsor"
+                  >
+                    <span className="font-sans break-words transition-colors font-semibold whitespace-nowrap select-auto text-base leading-1 lg:text-md lg:leading-2 text-white">
+                      Sponsor us!
+                    </span>
+                  </Link>
+                </div>
+              </div>
+              <div className="lg:col-span-6 md:col-span-6 lg:col-start-7 md:col-start-7 pt-10">
+                {Object.entries(SPONSORS_2024).map(([key, sponsors]) => (
+                  <div key={key}>
+                    <h2 className="font-sans font-semibold text-3md leading-8 lg:text-xl lg:leading-11 text-black">
+                      {key.charAt(0).toUpperCase() + key.slice(1)}
+                    </h2>
+                    <span className="h-2 block"></span>
+                    <div className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-[100%] gap-2 lg:gap-4">
+                      {sponsors.map((sponsor, index) => (
+                        <a
+                          key={index}
+                          className="bg-cream border border-black relative"
+                          href={sponsor.website}
+                          target="_blank"
+                          rel="noopener"
+                        >
+                          <div>
+                            <div className="pb-[50%] lg:pb-[60%]"></div>
+                            <div className="absolute inset-10 lg:inset-6">
+                              <Image
+                                src={sponsor.logo}
+                                alt={sponsor.name}
+                                layout="fill"
+                                objectFit="contain"
+                              />
+                            </div>
+                          </div>
+                        </a>
+                      ))}
+                    </div>
+                    <span className="h-8 lg:h-12 block"></span>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
+        <span className="h-16 lg:h-32 block"></span>
       </div>
     </section>
   );
