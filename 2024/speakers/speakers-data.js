21f5fa913e92549a4c8ff02f57c0da448b5b457f
HassanBahati <mukisabahati@gmail.com>
2024-10-08 13:21:49 +0300

    update speakers and keynotes

--- 2024/speakers/speakers-data.js
+++ 2024/speakers/speakers-data.js
@@ -1,118 +1,119 @@
 export const SPEAKERS_2024 = [
-	{
-		name: "Abraham Imani Bahati",
-		image: "/2024/speakers/Abraham_Imani_Bahati.png",
-	},
-	{
-		name: "Allan Kiche",
-		image: "/2024/speakers/Allan Kiche.png",
-	},
-	{
-		name: "Angufibo Lincoln",
-		image: "/2024/speakers/Angufibo Lincoln.png",
-	},
-	{
-		name: "Anthony Byansi",
-		image: "/2024/speakers/Anthony Byansi.png",
-	},
-	{
-		name: "Arthur Kakande",
-		image: "/2024/speakers/Arthur Kakande.png",
-	},
-	{
-		name: "Brayan Kai",
-		image: "/2024/speakers/Brayan Kai.png",
-	},
-	{
-		name: "Chris N. Achinga",
-		image: "/2024/speakers/Chris N. Achinga.png",
-	},
-	{
-		name: "Collins Mesue",
-		image: "/2024/speakers/Collins Mesue.png",
-	},
-	{
-		name: "Fanny Nyayic",
-		image: "/2024/speakers/Fanny Nyayic.png",
-	},
-	{
-		name: "Felix Jumason",
-		image: "/2024/speakers/Felix Jumason.png",
-	},
-	{
-		name: "Hassan Bahati",
-		image: "/2024/speakers/HAssan Bahati.png",
-	},
-	{
-		name: "Jonathan Ssali",
-		image: "/2024/speakers/Jonathan Ssali.png",
-	},
-	{
-		name: "Josh Ndemenge",
-		image: "/2024/speakers/Josh Ndemenge.png",
-	},
-	{
-		name: "Jovan Mwesigwa",
-		image: "/2024/speakers/Jovan Mwesigwa.png",
-	},
-	{
-		name: "Julius Boakye",
-		image: "/2024/speakers/Julius Boakye.png",
-	},
-	{
-		name: "KAFUI ALORDO",
-		image: "/2024/speakers/KAFUI ALORDO.png",
-	},
-	{
-		name: "Kayondo Edward",
-		image: "/2024/speakers/Kayondo Edward.png",
-	},
-	{
-		name: "OKUMU JUSTINE",
-		image: "/2024/speakers/OKUMU JUSTINE.png",
-	},
-	{
-		name: "Pauline Gechure",
-		image: "/2024/speakers/Pauline Gechure.png",
-	},
-	{
-		name: "Rashid",
-		image: "/2024/speakers/Rashid.png",
-	},
-	{
-		name: "Victoria Mutai",
-		image: "/2024/speakers/Victoria Mutai.png",
-	},
-	{
-		name: "Wesley Kambale",
-		image: "/2024/speakers/Wesley Kambale.png",
-	},
-	{
-		name: "Baker-Sentamu",
-		image: "/2024/speakers/baker-sentamu.png",
-	},
-	{
-		name: "John Panga",
-		image: "/2024/speakers/john panga.png",
-	},
-	{
-		name: "Kayongo Brian Johnson",
-		image: "/2024/speakers/kayongo brian johnson.png",
-	},
-	{
-		name: "Musitwa Edmond",
-		image: "/2024/speakers/musitwa edmond.png",
-	},
-	{
-		name: "Oliseh Genesis",
-		image: "/2024/speakers/Genesis.png",
-	},
-	{
-		name: "Zain Shabbir",
-		image: "/2024/speakers/Zain.png",
-	},
-	{
-		name: "Hussein Kizz",
-		image: "/2024/speakers/Hussein_Kizz.jpg",
-	},
+  {
+    name: "Abraham Imani Bahati",
+    image: "/2024/speakers/Abraham_Imani_Bahati.png",
+  },
+  {
+    name: "Allan Kiche",
+    image: "/2024/speakers/Allan Kiche.png",
+  },
+  {
+    name: "Angufibo Lincoln",
+    image: "/2024/speakers/Angufibo Lincoln.png",
+  },
+  {
+    name: "Anthony Byansi",
+    image: "/2024/speakers/Anthony Byansi.png",
+  },
+  {
+    name: "Arthur Kakande",
+    image: "/2024/speakers/Arthur Kakande.png",
+  },
+  {
+    name: "Brayan Kai",
+    image: "/2024/speakers/Brayan Kai.png",
+  },
+  {
+    name: "Chris N. Achinga",
+    image: "/2024/speakers/Chris N. Achinga.png",
+  },
+  {
+    name: "Collins Mesue",
+    image: "/2024/speakers/Collins Mesue.png",
+  },
+  {
+    name: "Fanny Nyayic",
+    image: "/2024/speakers/Fanny Nyayic.png",
+  },
+  {
+    name: "Felix Jumason",
+    image: "/2024/speakers/Felix Jumason.png",
+  },
+  {
+    name: "Hassan Bahati",
+    image: "/2024/speakers/HAssan Bahati.png",
+  },
+  {
+    name: "Jonathan Ssali",
+    image: "/2024/speakers/Jonathan Ssali.png",
+  },
+  {
+    name: "Josh Ndemenge",
+    image: "/2024/speakers/Josh Ndemenge.png",
+  },
+  {
+    name: "Jovan Mwesigwa",
+    image: "/2024/speakers/Jovan Mwesigwa.png",
+  },
+  {
+    name: "Julius Boakye",
+    image: "/2024/speakers/Julius Boakye.png",
+  },
+  {
+    name: "KAFUI ALORDO",
+    image: "/2024/speakers/KAFUI ALORDO.png",
+  },
+  {
+    name: "Kayondo Edward",
+    image: "/2024/speakers/Kayondo Edward.png",
+  },
+  {
+    name: "OKUMU JUSTINE",
+    image: "/2024/speakers/OKUMU JUSTINE.png",
+  },
+  {
+    name: "Pauline Gechure",
+    image: "/2024/speakers/Pauline Gechure.png",
+  },
+  {
+    name: "Rashid",
+    image: "/2024/speakers/Rashid.png",
+  },
+  {
+    name: "Victoria Mutai",
+    image: "/2024/speakers/Victoria Mutai.png",
+  },
+  {
+    name: "Wesley Kambale",
+    image: "/2024/speakers/Wesley Kambale.png",
+  },
+  {
+    name: "Baker-Sentamu",
+    image: "/2024/speakers/baker-sentamu.png",
+  },
+  {
+    name: "John Panga",
+    image: "/2024/speakers/john panga.png",
+  },
+  {
+    name: "Kayongo Brian Johnson",
+    image: "/2024/speakers/kayongo brian johnson.png",
+  },
+  {
+    name: "Musitwa Edmond",
+    image: "/2024/speakers/musitwa edmond.png",
+  },
+  {
+    name: "Oliseh Genesis",
+    image: "/2024/speakers/Genesis.png",
+  },
+  {
+    name: "Zain Shabbir",
+    image: "/2024/speakers/Zain.png",
+  },
+  {
+    name: "Hussein Kizz",
+    image: "/2024/speakers/Hussein_Kizz.jpg",
+  },
+  { name: "Sodiq Akinjobi", image: "/2024/speakers/Sodiq A.png" },
 ];
